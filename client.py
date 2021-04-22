9a866548bed5983a10e2b0d89796273210804da4
Cristian Gutu <gutucristian27@gmail.com>
2021-04-22 12:21:10 -0400

    update client.py

--- client.py
+++ client.py
@@ -10,12 +10,10 @@ user_data = {
   'ClientId' : '6p0to3eafoghamofcdv7mntf3g'
 }
 
-headers = {'Content-Type': 'application/x-amz-json-1.1', 'X-Amz-Target': 'AWSCognitoIdentityProviderService.InitiateAuth' }
+headers = {'Content-Type': 'application/x-amz-json-1.1', 'X-Amz-Target': 'AWSCognitoIdentityProviderService.InitiateAuth'}
 resp = requests.post('https://cognito-idp.us-east-1.amazonaws.com', data=json.dumps(user_data), headers=headers)
 
 token = json.loads(resp.content)['AuthenticationResult']['AccessToken']
-
-print(token)
 
 response = requests.get('https://api.gutucristian.com/ad_request?country=us&lang=eng', headers={'Authorization': token})
 
